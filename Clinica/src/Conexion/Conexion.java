2bc9f86726a13ea7eb690d1b6060e45de189aeb4
Choben18 <36686399+Choben18@users.noreply.github.com>
2018-02-20 22:10:16 -0600

    Importacion del proyecto
    Aqui esta todo el proyecto completo para que lo vayan checando

---
+++ Clinica/src/Conexion/Conexion.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conexion;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Angel
+ */
+public class Conexion {
+    public static final String tblusuarios ="usuarios";
+    public  static final String tblpacientes="pacientes";
+    public static Connection  getConnection(){
+        Connection conn =null;
+        try {
+            String url = "jdbc:mysql://localhost/";
+            String dbName= "clinica";
+            String userName = "root";
+            String password = "";
+            conn = DriverManager.getConnection(url+dbName,userName,password);
+            System.out.println(conn);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "CONEXION -> ERROR -> "+e);
+        }
+        return conn;
+    }
+    
+}
